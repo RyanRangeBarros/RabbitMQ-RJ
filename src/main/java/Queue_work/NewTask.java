1d4d409cc561b8e9ef266dc574f5ed6e72aaaaf4
RyanRangeBarros <rrwyabarros@gmail.com>
2022-05-23 05:43:25 -0300

    primeiro projeto rabbitMQ

---
+++ src/main/java/Queue_work/NewTask.java
@@ -0,0 +1,26 @@
+package Queue_work;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+import java.util.Scanner;
+public class NewTask {
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+        Scanner in = new Scanner(System.in);
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+            System.out.println("Digita sua msg");
+            String message = in.nextLine();
+
+            channel.basicPublish("", TASK_QUEUE_NAME,
+                    MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
